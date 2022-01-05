73b26e14ba89405a326724ea3b60591a42802bc7
Rafael <felippelli@live.com>
2022-01-05 19:01:40 -0300

    Criação de Primeiro programa em .Net

--- Dotnet/DigitalInovationOne/Program.cs
+++ Dotnet/DigitalInovationOne/Program.cs
@@ -2,16 +2,16 @@
 
 namespace DigitalInovationOne
 {
-
   class Program
   {
     static void Main(string[] args)
     {
       int numeroDeVezes = 5;
-      
+
       for (int i=0; i < numeroDeVezes; i++)
-    }
-      Console.WriteLine($"Bem Vindo ao Curso de .Net {i}");
+      {
+        Console.WriteLine($"Bem Vindos ao curso de .Net {i}");
+      }
     }
   }
 }
