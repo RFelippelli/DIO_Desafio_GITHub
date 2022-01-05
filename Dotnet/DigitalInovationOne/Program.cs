28ef9b8e84a83e2504006316988da41a88226337
Rafael <felippelli@live.com>
2022-01-05 18:39:32 -0300

    inicio das atividades em .net

---
+++ Dotnet/DigitalInovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int numeroDeVezes = 5;
+      
+      for (int i=0; i < numeroDeVezes; i++)
+    }
+      Console.WriteLine($"Bem Vindo ao Curso de .Net {i}");
+    }
+  }
+}
