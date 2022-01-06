d1a3941a6efbd4d29392054fc1f6f147d8ee47e6
Rafael <felippelli@live.com>
2022-01-06 20:14:53 -0300

    Primeiro exercício em C# voltado para as aulas de Programação Orientada a Objeto

---
+++ POO/ExemploPoo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExemploPoo.Models;
+
+namespace ExemploPoo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Pessoa p1 = new Pessoa();
+      
+      p1.Nome = "Bob";
+      p1.Idade = 20;
+
+      p1.Apresentar();
+    }
+  }
+}
